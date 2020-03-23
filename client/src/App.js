74849682e89c5e11073f2cd890de808a6359863d
Johan Bueno <johan.s.bueno@gmail.com>
2020-03-22 21:21:12 -0400

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import BooksContainer from "./pages/google";
--- client/src/pages/google.js
+++ client/src/pages/google.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from 'axios';
+import "./style.css"
 // import API from "../utils/API";
 // import Container from "../components/Container";
 // import Row from "../components/Row";
@@ -11,18 +12,18 @@ function BooksContainer () {
 
     const[book, setBook] = useState("");
     const[result, setResult] = useState([]);
-    const apiKey ="AIzaSyBugwoxoNOHl7gMi37WkI6Bh5_QZh4Ghx4";
+    const [apiKey,setApiKey] =useState("AIzaSyBugwoxoNOHl7gMi37WkI6Bh5_QZh4Ghx4");
     
-  
+   
   function handleChange(event) {
-
     const book = event.target.value;
     setBook(book);
   
   };
-   function handleSubmit(event) {
+   
+  function handleSubmit(event) {
     event.preventDefault();
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey + "&maxResults=20")
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey + "&maxResults=5")
       .then(data => {
         setResult(data.data.items)
         console.log(data.data.items)
@@ -30,7 +31,6 @@ function BooksContainer () {
       })
   
   }
-
   return (
 
     <div className="container">
@@ -40,16 +40,26 @@ function BooksContainer () {
           <input onChange={handleChange} className="form-control mt-10" placeholder="Search for Books" autoComplete="off" />
         </div>
         <button type="submit" className="btn btn-danger">Search </button>
+        <br></br>
+        <br></br>
+        <br></br>
       </form>
 
       {result.map(book => (
         <li key={book.id}>
 
           <div>
-          <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
-          <div > Info: {book.volumeInfo.title}</div>
+
+          <div >  {book.volumeInfo.title}</div>
+          <button>Save</button>
+          <a href={book.volumeInfo.infoLink}>
+          <button>View</button>
+          </a>
           <div > Authors: {book.volumeInfo.authors}</div>
-          <div > Description: {book.volumeInfo.description}</div>
+          <div>Published Date : {book.volumeInfo.publishedDate}</div>
+                   
+          <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
+          <div> Description: {book.volumeInfo.description}</div>
           </div>
              
           </li>
